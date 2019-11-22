8663466b77910230bc270a60cb72a48116cd9862
vijisiyanthan <vijisiyanthan@gmail.com>
2019-11-22 00:36:48 -0500

    Update User Edit Account Pages and Index Pages to implement roles

--- RoleAuth/Pages/Users/Edit.cshtml.cs
+++ RoleAuth/Pages/Users/Edit.cshtml.cs
@@ -1,8 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RoleAuth.Data;
 using RoleAuth.Utility;
@@ -13,28 +16,48 @@ namespace RoleAuth.Pages.Users
     public class EditModel : PageModel
     {
         private readonly ApplicationDbContext _db;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public EditModel(ApplicationDbContext db)
+        public EditModel(ApplicationDbContext db,UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
         {
             _db = db;
+            _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         [BindProperty]
-        public IdentityUser IdentityUser { get; set; }
+        public UpdateUserRoleViewModel IdentityUser { get; set; }
 
         public async Task<IActionResult> OnGetAsync(string id)
         {
+            var roles = await _db.Roles.ToListAsync();
+            var users = await _db.IdentityUser.FirstOrDefaultAsync(m => m.Id == id);
+
+
             if (id.Trim().Length == 0)
             {
                 return NotFound();
             }
 
-            IdentityUser = await _db.IdentityUser.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (IdentityUser == null)
+            if (users == null)
             {
                 return NotFound();
             }
+
+            var viewModel = new UpdateUserRoleViewModel
+            {
+                Id = id,
+                Email = users.Email,
+                selectedRole = _userManager.GetRolesAsync(users).Result[0],
+                Roles = roles.Select(x => new SelectListItem
+                {
+                    Value = x.NormalizedName,
+                    Text = x.NormalizedName
+                })
+            };
+            IdentityUser = viewModel;
             return Page();
         }
 
@@ -54,11 +77,27 @@ namespace RoleAuth.Pages.Users
                 else
                 {
                     userInDb.Email = IdentityUser.Email;                    
-
                     await _db.SaveChangesAsync();
+                    var previousrole = _userManager.GetRolesAsync(userInDb).Result[0];
+
+                    if (previousrole != IdentityUser.selectedRole)
+                    {
+                        await _userManager.AddToRoleAsync(userInDb, IdentityUser.selectedRole);
+                        await _userManager.RemoveFromRoleAsync(userInDb, previousrole);
+                    }
+
                     return RedirectToPage("Index");
                 }
             }
         }
+        public class UpdateUserRoleViewModel
+        {
+            public string Id { get; set; }
+            public string Email { get; set; }
+            public string selectedRole { get; set; }
+            public IEnumerable<SelectListItem> Roles { get; set; }
+        }
+
+
     }
 }
--- RoleAuth/Pages/Users/Index.cshtml.cs
+++ RoleAuth/Pages/Users/Index.cshtml.cs
@@ -16,21 +16,58 @@ namespace RoleAuth.Pages.Users
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _db;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public IndexModel(ApplicationDbContext db)
+
+        public IndexModel(ApplicationDbContext db, UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
         {
             _db = db;
+            _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         [BindProperty]
-        public List<IdentityUser> IdentityUserList { get; set; }
+        public List<EditModelUsers> IdentityUserList { get; set; }
 
 
         public async Task<IActionResult> OnGet()
         {
-            IdentityUserList = await _db.IdentityUser.ToListAsync();
+            var roles =await _db.Roles.ToListAsync();
+            var users =await  _db.Users.ToListAsync();
+            var userRoles =await  _db.UserRoles.ToListAsync();
+
+            var converteduser =  users.Select(x => new EditModelUsers
+            {
+                User = x.Email,
+                Id = x.Id,
+                Role = roles
+                    .Where(y => userRoles.Any(z => z.UserId == x.Id && z.RoleId == y.Id))
+                    .Select(y => new UsersRole
+                    {
+                        Name = y.NormalizedName
+                    })
+            });
+
+            IdentityUserList = converteduser.ToList();
+
 
             return Page();
         }
+
+        public class UsersRole
+        {
+            public string Name { get; set; }
+        }
+
+        public class EditModelUsers
+        {
+            public string Id { get; set; }
+            public string User { get; set; }
+            public IEnumerable<UsersRole> Role { get; set; }
+        }
+
+
     }
 }
